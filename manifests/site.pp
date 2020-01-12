13598e0c8cb38c17fd1d1b6d4485cfd2c6071ece
sgalan1980 <35709691+sgalan1980@users.noreply.github.com>
2020-01-12 23:28:20 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
     ensure => file,
-    content => 'This is a readme'
-    owner => 'root'
+    content => 'This is a readme',
+    owner => 'root',
   }
 }
